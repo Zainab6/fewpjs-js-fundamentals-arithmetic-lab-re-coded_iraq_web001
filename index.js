6721024a6ef7c1a78c4be89b51dfa00016cbec76
Zainab Saud <zai99nb@gmail.com>
2020-05-26 15:23:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const num1 = 2;
 const num2 = 31;
 const multiply =num1*num2;
 
-  Math.random
+
+Math.floor(Math.random() * 10);
   
   
 
-   let mod = 16 % 6 ;
+let mod = 16 % 6 ;
   
     
-  function max () {
-   const max = Math.max(12,4,6,20,7);
-    return max;
-   }
-   max()
+  
+const max = Math.max(12,4,6,20,7);
+
   
